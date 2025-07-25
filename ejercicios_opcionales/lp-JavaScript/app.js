edd4bd26311d542dd7b8638b1060df61558ef7dc
CharlieH52 <carlos.gomez.h52@gmail.com>
2025-07-25 13:05:32 -0600

    Reorganizacion de ejercicios opcionales

--- ejercicios_opcionales/lp-JavaScript/app.js
+++ ejercicios_opcionales/lp-JavaScript/app.js
@@ -60,40 +60,4 @@ function cash(){
 /*
 5.  Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
 */
-function concat_name(){
-    let userName = prompt("Ingresa tu nombre para acceder:");
-    alert("Bienvenido! " + userName);
-}
 
-let min = 1;
-let max = 10;
-while (min <= 10) {
-    console.log("Conteo actual: " + min);
-    min++;
-}
-
-let numStart = 10;
-let numEnd = 0;
-
-while (numStart >= numEnd) {
-    console.log("Contador hacia atras: " + numStart);
-    numStart--;
-}
-
-let temporizador = prompt("Asigna un valor al temporizador:");
-let tiempoFinal = 0;
-
-while (temporizador >= tiempoFinal) {
-    console.log("Cuenta actual: " + temporizador);
-    temporizador--;
-}
-
-let conteo = prompt("Digita el numero a contar:");
-let inicio = 0;
-
-while (inicio <= conteo) {
-    console.log("Pasos alcanzados: " + inicio + ", " + conteo);
-    inicio++;
-}
-
-conteo == 1 ? "a" : "b";
